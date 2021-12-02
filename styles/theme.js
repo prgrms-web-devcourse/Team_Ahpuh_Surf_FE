5e417b9ba8aebca998b7f1f51fa6e815416eca5c
KIMYEIM <kyaeim@gmail.com>
2021-12-02 13:48:16 +0900

    :sparkles:  Feat: ThemeProvider 적용 & theme 생성
    전역에 Emotion ThemeProvider 적용 & theme 예시 파일 생성
    
    Closes #29

---
+++ styles/theme.js
@@ -0,0 +1,13 @@
+const color = {
+  $main: '#918ae2',
+  $main__light: '#CBC8F1',
+  $main__dark: '#5D55B4',
+  $heart: '#EA4A4A',
+  $pink: '#FFDCE0',
+}
+
+const theme = {
+  color,
+}
+
+export default theme
