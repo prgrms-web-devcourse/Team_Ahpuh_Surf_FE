8be21570ebb03a8c60cb49541fd0cf5d66106f2f
rlacksals96 <rlacksals96@gmail.com>
2021-12-02 14:52:27 +0900

    :recycle:  Chore: 사진 업로드 할 수 있게 설정 추가
    Closes #7

---
+++ stories/hooks/useForm.stories.jsx
@@ -0,0 +1,58 @@
+import useForm from '../../hooks/useForm'
+
+export default {
+  title: 'Hook/useForm',
+}
+
+const sleep = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      return resolve()
+    }, 1000)
+  })
+}
+
+export const Default = () => {
+  const { isLoading, errors, handleSubmit, handleChange } = useForm({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: async (values) => {
+      await sleep()
+      alert(JSON.stringify(values))
+    },
+    validate: ({ email, password }) => {
+      if (!email) errors.email = 'input email'
+      if (!password) errors.password = 'input password'
+      if (!/^.+@.+\..+$/.test(email)) errors.email = 'input proper email style'
+      return errors
+    },
+  })
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Sign in</h1>
+      <div>
+        <input
+          name="email"
+          type="email"
+          placeholder="email"
+          onChange={handleChange}
+        />
+      </div>
+      <div>{errors.email}</div>
+      <div>
+        <input
+          name="password"
+          type="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
+      </div>
+      <div>{errors.password}</div>
+      <button type="submit" disabled={isLoading}>
+        {isLoading ? 'loading...' : 'submit'}
+      </button>
+    </form>
+  )
+}
