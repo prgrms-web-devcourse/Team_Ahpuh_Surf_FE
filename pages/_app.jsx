deedb9f46a7867edd84b467abbcdd78c0f62dd38
KIMYEIM <kyaeim@gmail.com>
2021-12-02 15:39:59 +0900

    :sparkles:  Feat: 전역에 Header 컴포넌트 적용
    Closes #11

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,12 @@
 import { Global, ThemeProvider } from '@emotion/react'
-import { Global } from '@emotion/react'
+import Header from '../components/common/Header'
 import theme from '../styles/theme'
 import reset from '../styles/reset'
 
 const MyApp = ({ Component, pageProps }) => (
   <ThemeProvider theme={theme}>
     <Global styles={reset} />
+    <Header />
     <Component {...pageProps} />
   </ThemeProvider>
 )
