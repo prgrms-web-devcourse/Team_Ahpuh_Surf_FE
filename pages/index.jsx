5fd185668884b6089f62e20d2c90f9d635f91b37
rlacksals96 <rlacksals96@gmail.com>
2021-12-02 14:52:27 +0900

    :recycle:  Chore: 사진 업로드 할 수 있게 설정 추가
    Closes #7

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,21 @@
 import styled from '@emotion/styled'
+import Avatar from 'components/base/Avatar'
 
 const HomeWrapper = styled.div`
   background-color: orange;
 `
 
 const Home = () => {
-  return <HomeWrapper>Hello</HomeWrapper>
+  return (
+    <div>
+      <HomeWrapper>Hello</HomeWrapper>
+      <Avatar
+        src="/../public/sample.jpeg"
+        alt="test"
+        width="100"
+        height="100"
+      />
+    </div>
+  )
 }
 export default Home
