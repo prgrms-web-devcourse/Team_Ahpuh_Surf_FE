b7db113597409407f9905498cb58fbf9712592c9
Jeongs <kimm.jeongs@gmail.com>
2021-12-03 16:25:35 +0900

    :sparkles:  Feat: Navbar 구현
    Closes #58

---
+++ stories/common/Navbar.stories.jsx
@@ -0,0 +1,9 @@
+import { Navbar } from 'components/common'
+
+export default {
+  title: 'common/Navbar',
+  component: Navbar,
+}
+
+export const Default = () => <Navbar />
+export const Color = () => <Navbar color="#373530" />
