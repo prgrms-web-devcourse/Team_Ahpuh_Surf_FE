f5fffa5942775c2e19534f9618ef2c77961b16b9
KIMYEIM <kyaeim@gmail.com>
2021-12-05 16:12:37 +0900

    :lipstick:  UI: Storybook에 theme 적용
    ThemeProvider, addDecorator 사용
    
    Closes #93

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,9 @@
+import { ThemeProvider } from 'emotion-theming'
+import theme from '../styles/theme'
+import { addDecorator } from '@storybook/react'
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
@@ -7,3 +11,8 @@ export const parameters = {
     },
   },
 }
+
+const EmotionThemeProvider = (storyFn) => (
+  <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>
+)
+addDecorator(EmotionThemeProvider)
