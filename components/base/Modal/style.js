3789a719fd4a7be474821f0c0f2469c8681c9197
Jeongs <kimm.jeongs@gmail.com>
2021-12-05 15:26:40 +0900

    :lipstick:  UI: Modal padding 수정, closest null 수정
    Closes #88

--- components/base/Modal/style.js
+++ components/base/Modal/style.js
@@ -18,7 +18,7 @@ export const ModalContainer = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  padding: 32px;
+  padding-top: ${({ closeBtn }) => closeBtn && '32px'};
   border-radius: 8px;
   background-color: white;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.2);
@@ -30,8 +30,8 @@ export const ModalContainer = styled.div`
 `
 export const ModalClose = styled.button`
   position: absolute;
-  top: 10px;
-  right: 10px;
+  top: 0;
+  right: 0;
   display: inline-block;
   cursor: pointer;
   background: none;
