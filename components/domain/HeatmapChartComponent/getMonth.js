47b0be3f46e83643db89ceb08d063f816f3387f5
rlacksals96 <rlacksals96@gmail.com>
2021-12-07 02:19:23 +0900

    :sparkles:  Feat: Heatmap 컴포넌트 구현
    Closes #108

---
+++ components/domain/HeatmapChartComponent/getMonth.js
@@ -0,0 +1,29 @@
+export const getMonth = (date) => {
+  switch (new Date(date).getMonth()) {
+    case 0:
+      return 'Jan'
+    case 1:
+      return 'Feb'
+    case 2:
+      return 'Mar'
+    case 3:
+      return 'Apr'
+    case 4:
+      return 'May'
+    case 5:
+      return 'Jun'
+    case 6:
+      return 'Jul'
+    case 7:
+      return 'Aug'
+    case 8:
+      return 'Sep'
+    case 9:
+      return 'Oct'
+    case 10:
+      return 'Nov'
+    case 11:
+      return 'Dec'
+    default:
+  }
+}
