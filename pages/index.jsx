6ac258be74d42e3265d9458b328bf79bb7e78b7e
rlacksals96 <rlacksals96@gmail.com>
2021-12-06 14:26:47 +0900

    :bug:  Fix: x축 데이터 값 제대로 랜더링 안되는 문제 해결
    Closes #91

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,24 +14,9 @@ const AreaChart = dynamic(
 )
 // TODO: data 받으면 sampleXNY1 형태처럼 컨버팅 하고, dataset으로 만들어주는 hook 제작
 
-const datas = sampleData.map(({ x, y }) => [
-  Number(`${new Date(x).getTime()}`),
-  Number(`${y}`),
-])
-const datas2 = sampleData2.map(({ x, y }) => [
-  Number(`${new Date(x).getTime()}`),
-  Number(`${y}`),
-])
-
-// console.log(datas)
 const dataset = []
-dataset.push({ data: datas, name: 'react' })
-dataset.push({ data: datas2, name: 'Vue' })
-// dataset.push({ data: sampleData, name: 'react' })
-// dataset.push({ data: sampleData2, name: 'Vue' })
-// console.log(dataset)
-// console.log(dataset[0].data[dataset[0].data.length - 1].x)
-// console.log({ data: sampleData, name: 'react' })
+dataset.push({ data: sampleData, name: 'react' })
+dataset.push({ data: sampleData2, name: 'Vue' })
 const Home = () => (
   <div>
     <HomeWrapper>Hello</HomeWrapper>
