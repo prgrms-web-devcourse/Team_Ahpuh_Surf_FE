0d0f31515d517d6bfa0dc90049834b518f5f28aa
KIMYEIM <kyaeim@gmail.com>
2021-12-07 15:08:17 +0900

    :lipstick:  UI: 404 페이지 마크업
    Closes #115

---
+++ pages/404/style.js
@@ -0,0 +1,42 @@
+import styled from "@emotion/styled"
+
+export const NotFoundWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 100%;
+  overflow: hidden;
+`
+
+export const NotFoundMsg = styled.div`
+  height: 130px;
+  font-weight: 900;
+  text-align: center;
+  line-height: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  font-size: 22px;
+  flex-shrink: 0;
+  position: absolute;
+
+  /* & > p {
+    font-size: 22px;
+  } */
+`
+
+export const LottieWrapper = styled.div`
+  background-color: skyblue;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 800px;
+  height: 700px;
+  margin-top: 100px;
+`
+
+export const LinkWrapper = styled.div`
+  /* font-size: 24px; */
+`
