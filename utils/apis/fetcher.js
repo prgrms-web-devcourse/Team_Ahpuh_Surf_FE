d73944c7b25af6688ee40f64c729e86253f884ab
Jeongs <kimm.jeongs@gmail.com>
2021-12-09 21:49:26 +0900

    :sparkles:  Feat: SWR fetcher 함수 …
    Closes #129

---
+++ utils/apis/fetcher.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const fetcher = (url) =>
+  axios
+    .get(url, {
+      withCredentials: true,
+    })
+    .then((response) => response.data)
+
+export default fetcher
