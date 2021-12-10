7f668dbe4ce89a4219c616c86dc976e2693e8dc3
rlacksals96 <rlacksals96@gmail.com>
2021-12-10 22:50:46 +0900

    :lipstick:  UI: mypage edit UI 마크업
    Closes #131

---
+++ pages/mypage/edit/style.js
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled'
+
+export const Container = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-left: 10px;
+  margin-right: 10px;
+`
+export const ButtonPlus = styled.div`
+  display: flex;
+  position: relative;
+  bottom: 80px;
+  left: 120px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 20px;
+  width: 40px;
+  height: 40px;
+  background-color: black;
+  color: white;
+  font-size: 38px;
+  cursor: pointer;
+`
+export const ButtonSubmit = styled.button`
+  position: absolute;
+  bottom: 80px;
+  width: 95%;
+  height: 35px;
+  border: none;
+  border-radius: 4px;
+  background-color: #c1ddeb;
+  font-size: 20px;
+`
