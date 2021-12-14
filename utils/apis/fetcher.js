94e0e21f9573681212d980ff7538e3f1a7200d32
Jeongs <kimm.jeongs@gmail.com>
2021-12-14 17:02:00 +0900

    :bug:  Fix: CORS 해결
    Closes #174

--- utils/apis/fetcher.js
+++ utils/apis/fetcher.js
@@ -1,10 +1,7 @@
 import axios from 'axios'
+import { API_END_POINT } from 'constants/environment'
 
 const fetcher = (url) =>
-  axios
-    .get(url, {
-      withCredentials: true,
-    })
-    .then((response) => response.data)
+  axios.get(`${API_END_POINT}/${url}`).then((response) => response.data)
 
 export default fetcher
