8b2861ad722ea6420d34dee985fc28e46a43b818
Jeongs <kimm.jeongs@gmail.com>
2021-12-14 17:42:33 +0900

    :bug:  Fix: withCredential: true in fetcher 함수
    Closes #176

--- utils/apis/fetcher.js
+++ utils/apis/fetcher.js
@@ -2,6 +2,10 @@ import axios from 'axios'
 import { API_END_POINT } from 'constants/environment'
 
 const fetcher = (url) =>
-  axios.get(`${API_END_POINT}/${url}`).then((response) => response.data)
+  axios
+    .get(`${API_END_POINT}/${url}`, {
+      withCredentials: true,
+    })
+    .then((response) => response.data)
 
 export default fetcher
