1616ba56b60785beb341c47ad2d120090647e8da
KIMYEIM <kyaeim@gmail.com>
2021-12-15 18:30:33 +0900

    :lipstick:  UI: 둘러보기 페이지 마크업 & 스타일 수정
    Closes #184

--- pages/explore/index.jsx
+++ pages/explore/index.jsx
@@ -1,51 +1,51 @@
 import styled from '@emotion/styled'
-import Link from 'next/link'
 import dayjs from 'dayjs'
-import { Children, useEffect } from 'react'
+import { Children } from 'react'
 import { Post } from 'components/domain'
 import { Explores } from 'utils/SampleData/Explores'
 
 const Container = styled.div`
   padding: 0 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  height: 100%;
+  overflow-y: auto;
 `
 
-const Explore = () => {
-  const test = 1
+const Explore = () => (
+  <Container>
+    {Children.toArray(
+      Explores.map(({ user, post }) => {
+        const month = dayjs(post.selectedDate).format('M')
 
-  return (
-    <Container>
-      {Children.toArray(
-        Explores.map(({ user, post }) => {
-          const month = dayjs(post.selectedDate).format('M')
-
-          return (
-            // <Link key={user.userId} href={`/posts/${month}/${post.postId}`}>
-            //   <a
-            //     style={{
-            //       display: 'block',
-            //       backgroundColor: 'red',
-            //       // textDecoration: 'none',
-            //     }}>
-            <Post
-              isMine={false}
-              profileImage={user.profileUrl ? user.profileUrl : undefined}
-              username={user.username}
-              follow={user.follow}
-              date={post.selectedDate}
-              createdAt={post.createdAt}
-              categoryName={post.categoryName}
-              content={post.content}
-              score={post.score}
-              backgroundColor={post.colorCode}
-              key={user.userId}
-            />
-            //   </a>
-            // </Link>
-          )
-        }),
-      )}
-    </Container>
-  )
-}
+        return (
+          // <Link key={user.userId} href={`/posts/${month}/${post.postId}`}>
+          //   <a
+          //     style={{
+          //       display: 'block',
+          //       backgroundColor: 'red',
+          //       // textDecoration: 'none',
+          //     }}>
+          <Post
+            isMine={false}
+            profileImage={user.profileUrl ? user.profileUrl : undefined}
+            username={user.username}
+            follow={user.follow}
+            date={post.selectedDate}
+            createdAt={post.createdAt}
+            categoryName={post.categoryName}
+            content={post.content}
+            score={post.score}
+            backgroundColor={post.colorCode}
+            key={user.userId}
+          />
+          //   </a>
+          // </Link>
+        )
+      }),
+    )}
+  </Container>
+)
 
 export default Explore
