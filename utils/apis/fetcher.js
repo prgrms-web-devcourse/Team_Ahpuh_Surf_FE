baf3fd5db7a6820f0b41d72daebc9fda2e683c40
Jeongs <kimm.jeongs@gmail.com>
2021-12-16 02:22:52 +0900

    :bug:  Fix: header Authozation -> token
    Closes #197

--- utils/apis/fetcher.js
+++ utils/apis/fetcher.js
@@ -1,10 +1,14 @@
 import axios from 'axios'
-import { API_END_POINT } from 'constants/environment'
+import { API_END_POINT, LOGIN_COOKIE } from 'constants/environment'
+import Cookies from 'js-cookie'
 
 const fetcher = (url) =>
   axios
     .get(`${API_END_POINT}${url}`, {
       withCredentials: true,
+      headers: {
+        token: `${JSON.parse(Cookies.get(LOGIN_COOKIE))?.token}`,
+      },
     })
     .then((response) => response.data)
 
