655ca610e415f1575af515edf769cb9254da94b7
Jiyoung Kim <kimm.jeongs@gmail.com>
2021-12-17 19:11:31 +0900

    Wip: posts/new API 대기중 (#207)
    * :recycle:  Refactor: Textarea 컴포넌트 onChange 인자값 변경
    
    Closes #180
    
    * :construction:  Wip: 변경될 API 대기중
    
    Closes #180
    
    * :sparkles:  Feat: post 등록 api 적용
    
    Closes #180
    
    * :sparkles:  Feat: Dropdown 컴포넌트  isNew prop 추가 (#211)
    
    Closes #210
    
    * :recycle:  Refactor: try, catch 적용, 카테고리 등록 api 적용
    
    Closes #216
    
    * :sparkles:  Feat: post 등록 API 적용, try, catch 적용
    
    Closes #180
    
    * :sparkles:  Feat: API path 통일화
    
    Closes #215
    
    * :fire:  Prune: 안쓰는 코드 삭제 in AddSurfModal
    
    Closes #180

--- utils/apis/category/useGetCategories.js
+++ utils/apis/category/useGetCategories.js
@@ -1,8 +1,9 @@
 import useSWR from 'swr'
 import fetcher from 'utils/apis/fetcher'
+import { useGetCategoriesPath } from 'constants/apiPath'
 
 const useGetCategories = (options = {}) => {
-  const { data = {}, error } = useSWR('/categories', fetcher, options)
+  const { data = [], error } = useSWR(useGetCategoriesPath, fetcher, options)
 
   return {
     data,
