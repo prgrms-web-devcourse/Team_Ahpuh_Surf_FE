d63c1d605d9348c658a2b661242d00d14fbc9f7d
Jeongs <kimm.jeongs@gmail.com>
2021-12-18 15:29:16 +0900

    :sparkles:  Feat: 카테고리 관리 페이지 API 연동
    Closes #220

--- components/domain/CategoryCard/index.jsx
+++ components/domain/CategoryCard/index.jsx
@@ -1,46 +1,71 @@
 import styled from '@emotion/styled'
 import PropTypes from 'prop-types'
+import { deleteCategory } from 'utils/apis/category'
+import { UpdateCategoryModal } from 'components/domain'
+import { useToggle } from 'hooks'
+import { mutate } from 'swr'
 
-const CategoryCard = ({ title, categoryId }) => {
-  const CardContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    height: 100px;
-    padding: 10px 20px;
-    border: 1px solid black;
-    border-radius: 10px;
-  `
-  const ControlBox = styled.div`
-    span {
-      margin-left: 5px;
-      margin-right: 5px;
-      color: #3d7cd0;
-      font-size: 15px;
-      &:hover {
-        text-decoration: underline;
-        cursor: pointer;
-      }
+const CardContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: ${({ height }) =>
+    typeof height === 'number' ? `${height}px` : height};
+  padding: 10px;
+  border: 1px solid black;
+  border-radius: 10px;
+`
+const ControlBox = styled.div`
+  flex-shrink: 0;
+  button {
+    padding: 0 5px;
+    color: #3d7cd0;
+    font-size: 15px;
+    background-color: transparent;
+    border: none;
+    &:hover {
+      text-decoration: underline;
+      cursor: pointer;
     }
-  `
-  const Title = styled.div`
-    font-size: 25px;
-    font-weight: bold;
-  `
-
-  const handleEdit = () => {
-    console.log(`show edit modal. CategoryId: ${categoryId}`)
   }
-  const handleRemove = () => {
-    console.log(`show remove modal CategoryId: ${categoryId}`)
+`
+const Title = styled.h1`
+  font-size: ${({ fontSize }) =>
+    typeof fontSize === 'number' ? `${fontSize}px` : fontSize};
+  font-weight: bold;
+  white-space: ${({ ellipsis }) => ellipsis && 'nowrap'};
+  overflow: ${({ ellipsis }) => ellipsis && 'hidden'};
+  text-overflow: ${({ ellipsis }) => ellipsis && 'ellipsis'};
+`
+const CategoryCard = ({ title, categoryId, height, fontSize, ellipsis }) => {
+  const [toggleEdit, onToggleEdit, setToggleEdit] = useToggle(false)
+
+  const handleRemove = async () => {
+    try {
+      await deleteCategory(categoryId)
+      mutate('/categories')
+    } catch (error) {
+      console.error(error.message)
+    }
   }
   return (
-    <CardContainer>
-      <Title>{title}</Title>
+    <CardContainer height={height}>
+      <UpdateCategoryModal
+        toggleModal={toggleEdit}
+        setToggleModal={setToggleEdit}
+        categoryId={categoryId}
+      />
+      <Title fontSize={fontSize} ellipsis={ellipsis}>
+        {title}
+      </Title>
       <ControlBox>
-        <span onClick={handleEdit}>Edit</span>
-        <span onClick={handleRemove}>Remove</span>
+        <button type="button" onClick={onToggleEdit}>
+          Edit
+        </button>
+        <button type="button" onClick={handleRemove}>
+          Remove
+        </button>
       </ControlBox>
     </CardContainer>
   )
@@ -48,6 +73,15 @@ const CategoryCard = ({ title, categoryId }) => {
 
 CategoryCard.propTypes = {
   title: PropTypes.string.isRequired,
-  categoryId: PropTypes.string.isRequired,
+  categoryId: PropTypes.number.isRequired,
+  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  fontSize: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  ellipsis: PropTypes.bool,
+}
+
+CategoryCard.defaultProps = {
+  height: '100px',
+  fontSize: '25px',
+  ellipsis: false,
 }
 export default CategoryCard
--- pages/categorymanage/index.jsx
+++ pages/categorymanage/index.jsx
@@ -1,47 +1,65 @@
 import { AiOutlinePlus } from 'react-icons/ai'
 import styled from '@emotion/styled'
-import Link from 'next/link'
-import { data } from 'utils/SampleData/CategoryManage'
-import { CategoryCard } from 'components/domain'
+import { CategoryCard, AddSurfModal } from 'components/domain'
+import { useGetCategories } from 'utils/apis/category'
+import { useToggle } from 'hooks'
 
-const CategoryManage = () => {
-  const Main = styled.div`
-    padding: 20px;
-  `
-  const Top = styled.div`
-    display: flex;
-    justify-content: space-between;
-  `
-  const Body = styled.div`
-    margin-top: 20px;
-    overflow: scroll;
-    & * {
-      margin-top: 10px;
-      margin-bottom: 10px;
-    }
-  `
-  const handlePlus = () => {
-    console.log('plus')
+const Main = styled.div`
+  height: 100%;
+  padding: 20px;
+`
+const Top = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+const Body = styled.div`
+  margin-top: 20px;
+  overflow: scroll;
+  & * {
+    margin-top: 10px;
+    margin-bottom: 10px;
   }
-  // TODO: 서프 생성 모달 완성시 해당 링크 추가하기
+`
+const EmptyCategory = styled.p`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  font-size: 18px;
+`
+const CategoryManage = () => {
+  const { data: categories, isLoading: categoriesLoading } = useGetCategories({
+    revalidateOnFocus: false,
+  })
+  const [togglePlus, onTogglePlus, setTogglePlus] = useToggle(false)
 
   return (
     <Main>
+      <AddSurfModal toggleModal={togglePlus} setToggleModal={setTogglePlus} />
       <Top>
         <div style={{ fontSize: 20, fontWeight: 'bold' }}>Surf Manager</div>
-        {/* eslint-disable-next-line @next/next/link-passhref */}
-        <Link href="/">
-          <AiOutlinePlus
-            size={25}
-            style={{ cursor: 'pointer' }}
-            onClick={handlePlus}
-          />
-        </Link>
+        <AiOutlinePlus
+          size={25}
+          style={{ cursor: 'pointer' }}
+          onClick={onTogglePlus}
+        />
       </Top>
       <Body>
-        {data.map(({ categoryId, name }) => (
-          <CategoryCard categoryId={categoryId} title={name} />
-        ))}
+        {categories.length > 0 ? (
+          categories.map(({ categoryId, name }) => (
+            <CategoryCard
+              key={categoryId}
+              categoryId={categoryId}
+              title={name}
+              height={70}
+              fontSize={18}
+              ellipsis
+            />
+          ))
+        ) : (
+          <EmptyCategory>Category does not exist 🥲</EmptyCategory>
+        )}
       </Body>
     </Main>
   )
