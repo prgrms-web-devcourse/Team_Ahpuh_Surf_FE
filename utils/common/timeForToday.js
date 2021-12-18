c0c01d8efbb4339180489b31ac792897650e5662
Jeongs <kimm.jeongs@gmail.com>
2021-12-19 00:03:30 +0900

    :sparkles:  Feat: timeForToday 함수 구
    Closes #219

---
+++ utils/common/timeForToday.js
@@ -0,0 +1,16 @@
+export const timeForToday = (createdAt) => {
+  const today = new Date()
+  const createdTime = new Date(createdAt)
+
+  const betweenTime = Math.floor(
+    (today.getTime() - createdTime.getTime()) / 1000 / 60,
+  )
+  const betweenTimeHour = Math.floor(betweenTime / 60)
+  const betweenTimeDay = Math.floor(betweenTime / 60 / 24)
+
+  if (betweenTime < 1) return '방금 전'
+  if (betweenTime < 60) return `${betweenTime}분 전`
+  if (betweenTimeHour < 24) return `${betweenTimeHour}시간 전`
+  if (betweenTimeDay < 365) return `${betweenTimeDay}일 전`
+  return `${betweenTimeDay / 365}년 전`
+}
