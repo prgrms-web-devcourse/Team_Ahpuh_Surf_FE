46b9941651e102eee2e6cb56105db251e035c349
Jeongs <kimm.jeongs@gmail.com>
2021-12-18 15:30:23 +0900

    :fire:  Prune: 안쓰는 코드 삭제
    Closes #220

--- utils/common/shuffle.js
+++ utils/common/shuffle.js
@@ -1,4 +1,1 @@
-export const shuffle = (array) => {
-  console.log(array, 'array')
-  return array.sort(() => Math.random() - 0.5)[0]
-}
+export const shuffle = (array) => array.sort(() => Math.random() - 0.5)[0]
