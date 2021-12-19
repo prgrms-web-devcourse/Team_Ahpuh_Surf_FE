71e26c941e29a8684d437555750affe285733c8b
rlacksals96 <rlacksals96@gmail.com>
2021-12-19 15:08:22 +0900

    :sparkles:  Feat: options 추가& 포커스시 리랜더링 옵션 제거하여 불필요한 리랜더링 방어
    Closes #238

--- utils/apis/post/useGetPostAll.js
+++ utils/apis/post/useGetPostAll.js
@@ -1,10 +1,11 @@
 import useSWR from 'swr'
 import fetcher from 'utils/apis/fetcher'
 
-const useGetPost = (userId, cursorId) => {
+const useGetPost = (userId, cursorId, options) => {
   const { data = {}, error } = useSWR(
     `/posts/all?userId=${userId}&cursorId=${cursorId}`,
     fetcher,
+    options,
   )
   return {
     data,
