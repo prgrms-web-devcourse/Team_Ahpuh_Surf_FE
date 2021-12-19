cda5ab341a319219c85f3c18d1ecfe1d4627b24e
rlacksals96 <rlacksals96@gmail.com>
2021-12-19 15:05:13 +0900

    :recycle:  Refactor:dummy data제거, area chart module로 변경
    Closes #238

--- pages/mypage/index.jsx
+++ pages/mypage/index.jsx
@@ -2,7 +2,7 @@ import dynamic from 'next/dynamic'
 import { AiTwotoneSetting } from 'react-icons/ai'
 import { BsFillBellFill, BsFillPencilFill } from 'react-icons/bs'
 import Link from 'next/link'
-import { MainDropdown, Text } from 'components/base'
+import { Text } from 'components/base'
 import ContentBox from 'components/domain/ContentBox'
 import Profile from 'components/domain/Profile'
 import { useEffect, useState } from 'react'
@@ -12,20 +12,17 @@ import SkeletonBox from 'components/domain/SkeletonBox'
 import useGetUser from 'utils/apis/user/useGetUser'
 import FollowModal from 'components/domain/FollowModal'
 import useGetPostsCountYear from 'utils/apis/post/useGetPostsCountYear'
+import { useRouter } from 'next/router'
 import * as Style from './style'
 import { useToggle } from '../../hooks'
-import { useGetPostAll, useGetYearScore } from '../../utils/apis/post'
-import { useGetCategories } from '../../utils/apis/category'
+import AreaChartModule from '../../components/domain/AreaChartModule'
 
 const Mypage = () => {
-  const AreaChartComponent = dynamic(
-    import('components/domain/AreaChartComponent'),
-    { ssr: false },
-  )
   const HeatmapComponent = dynamic(
     import('components/domain/HeatmapChartComponent'),
     { ssr: false },
   )
+  const router = useRouter()
   const [toggleTabs, setToggleTabs] = useToggle(false)
   const [visible, setVisible] = useState(false)
 
@@ -34,66 +31,13 @@ const Mypage = () => {
     const { userId } = JSON.parse(Cookies.get('user'))
     setUid(userId)
   }, [])
-  const { data } = useGetUser(uId)
+  const { data: profileData } = useGetUser(uId)
   const { data: heatmapData } = useGetPostsCountYear(
     new Date().getFullYear(),
     uId,
+    { revalidateOnFocus: false },
   )
 
-  const { data: categories } = useGetCategories()
-  const { data: surfData } = useGetYearScore(uId)
-  const { data: allPosts } = useGetPostAll(uId, 0)
-
-  const [dataset, setDataset] = useState([])
-  const [selectedSurf, setSurf] = useState({ categoryId: null, name: 'All' })
-  const [catList, setCatList] = useState([])
-
-  useEffect(() => {
-    if (surfData && surfData.length !== 0) {
-      const allData = surfData?.map((surf) => ({
-        data: surf.postScores,
-        name: surf.categoryName,
-      }))
-      setDataset(allData)
-    }
-  }, [surfData, allPosts])
-
-  useEffect(() => {
-    if (categories && categories.length !== 0) {
-      setCatList([
-        {
-          categoryId: null,
-          name: 'All',
-        },
-        ...categories,
-      ])
-    }
-  }, [categories])
-
-  useEffect(() => {
-    console.log(selectedSurf)
-  }, [selectedSurf])
-
-  const handleClick = (item) => {
-    setSurf(item)
-    if (!item.categoryId) {
-      const allData = surfData.map((surf) => ({
-        data: surf.postScores,
-        name: surf.categoryName,
-      }))
-      setDataset(allData)
-    } else {
-      const result = surfData.filter(
-        (surf) => surf.categoryId === item.categoryId,
-      )
-      setDataset([
-        {
-          data: result[0].postScores,
-          name: result[0].categoryName,
-        },
-      ])
-    }
-  }
   const toggle = () => {
     setVisible(!visible)
   }
@@ -104,7 +48,7 @@ const Mypage = () => {
     console.log('click notice')
   }
 
-  if (!data || !uId) {
+  if (!profileData || !uId) {
     return <p />
   }
   return (
@@ -114,14 +58,15 @@ const Mypage = () => {
         toggleTabs={toggleTabs}
         setToggleTabs={setToggleTabs}
       />
-      <EditAboutMe userData={data} visible={visible} toggle={toggle} />
+      <EditAboutMe userData={profileData} visible={visible} toggle={toggle} />
       <div style={{ display: 'flex', justifyContent: 'end' }}>
-        <Link href="/mypage/edit">
-          <AiTwotoneSetting
-            size={30}
-            style={{ marginRight: 5, cursor: 'pointer' }}
-          />
-        </Link>
+        <AiTwotoneSetting
+          size={30}
+          style={{ marginRight: 5, cursor: 'pointer' }}
+          onClick={() => {
+            router.push('/mypage/edit')
+          }}
+        />
         <BsFillBellFill
           size={30}
           style={{ marginLeft: 5, cursor: 'pointer' }}
@@ -130,20 +75,20 @@ const Mypage = () => {
       </div>
       <Profile
         profilePhotoUrl={
-          data?.profilePhotoUrl === null
+          profileData?.profilePhotoUrl === null
             ? 'https://picsum.photos/200'
-            : data?.profilePhotoUrl
+            : profileData?.profilePhotoUrl
         }
-        userName={data?.userName}
-        email={data?.email}
+        userName={profileData?.userName}
+        email={profileData?.email}
       />
       <Style.FollowContainer onClick={() => toggleFollowModal()}>
         <Style.FollowItem>
-          <Text size={36}>{data?.followerCount}</Text>
+          <Text size={36}>{profileData?.followerCount}</Text>
           <Text size={20}>Follower</Text>
         </Style.FollowItem>
         <Style.FollowItem>
-          <Text size={36}>{data?.followingCount}</Text>
+          <Text size={36}>{profileData?.followingCount}</Text>
           <Text size={20}>Following</Text>
         </Style.FollowItem>
       </Style.FollowContainer>
@@ -157,42 +102,20 @@ const Mypage = () => {
           />
         </Style.Title>
         <Style.Title>URL: </Style.Title>
-        {data?.url ? (
-          <Style.Title>{data?.url}</Style.Title>
+        {profileData?.url ? (
+          <Style.Title>{profileData?.url}</Style.Title>
         ) : (
           <Style.Title style={{ fontSize: 20, color: '#8D8D8D' }}>
             추가해보세요
           </Style.Title>
         )}
-        <Style.Content>{data?.aboutMe}</Style.Content>
+        <Style.Content>{profileData?.aboutMe}</Style.Content>
       </Style.Introduction>
       <Style.Graph style={{ width: '100%', height: 350 }}>
         <Link href="/">
           <Style.Title>{`Main >`}</Style.Title>
         </Link>
-        <MainDropdown
-          height={30}
-          fontSize={15}
-          width={100}
-          selected={selectedSurf}
-          handleClick={handleClick}
-          data={catList}
-          isObj
-          border
-          style={{ marginTop: 10 }}
-        />
-        <div style={{ position: 'relative', top: 15 }}>
-          <Style.AreaChartSkeleton>
-            <Text size={30} color="darkGray">
-              Loading
-            </Text>
-          </Style.AreaChartSkeleton>
-          <AreaChartComponent
-            data={dataset}
-            isMyPage
-            style={{ marginTop: 10 }}
-          />
-        </div>
+        <AreaChartModule userId={uId} isMyPage={false} />
       </Style.Graph>
 
       <Link href="/dashboard">
