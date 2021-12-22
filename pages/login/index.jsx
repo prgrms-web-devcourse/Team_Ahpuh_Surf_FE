44f6d7147d734c2c2e2c26b8fcac96294ea1e778
Jeongs <kimm.jeongs@gmail.com>
2021-12-22 23:28:35 +0900

    :bug:  session await 삭제

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -28,7 +28,7 @@ const Login = () => {
           expires: 28,
           secure: true,
         })
-        await sessionStorage.setItem('welcome', true)
+        sessionStorage.setItem('welcome', true)
         router.push('/')
       } catch (error) {
         console.error(error.message)
